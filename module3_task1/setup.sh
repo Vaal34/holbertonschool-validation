c60323d650af1ceb33931f5c4f7cc35de4143329
Valentin <5619@holbertonstudents.com>
2023-05-16 06:31:35 -0700

    modifie makefile and setup.sh

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -1,4 +1,7 @@
-#!/bin/bash
-curl -L https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.deb -o hugo.deb
-apt install ./hugo.deb
-rm hugo.deb
+sudo apt-get update && sudo apt-get install -y hugo make git wget
+wget https://github.com/gohugoio/hugo/releases/download/v0.111.3/hugo_0.111.3_Linux-64bit.tar.gz
+tar -xf hugo_0.111.3_Linux-64bit.tar.gz
+sudo mv ./hugo /usr/bin/
+sudo rm hugo_0.111.3_Linux-64bit.tar.gz
+sudo npm install -g markdownlint-cli
+sudo npm install -g markdown-link-check
