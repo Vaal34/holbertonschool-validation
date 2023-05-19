130aa9fa83310866675627ebd0d9be6d70c4f0f0
Valentin <5619@holbertonstudents.com>
2023-05-19 03:39:23 -0700

    push all

--- module3_task3/go.mod
+++ module3_task3/go.mod
@@ -1,5 +1,5 @@
-module github.com/Vaal34/awesome-api
+module github.com/AlexandreGRN/awesome-api
 
-go 1.20
+go 1.15
 
 require github.com/gorilla/mux v1.8.0
--- module3_task3/setup.sh
+++ module3_task3/setup.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
-sudo apt-get install jq -y
-sudo wget https://github.com/mikefarah/yq/releases/download/v4.5.0/yq_linux_amd64.tar.gz
-sudo tar xzf yq_linux_amd64.tar.gz
-sudo mv yq_linux_amd64 yq
-sudo mv yq /usr/bin/
-sudo yq --version
-sudo curl -L https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.deb -o hugo.deb
-sudo apt install ./hugo.deb
-sudo rm hugo.deb
-sudo rm yq_linux_amd64.tar.gz
+sudo apt-get update && sudo apt-get install -y hugo make git wget
 wget https://github.com/gohugoio/hugo/releases/download/v0.111.3/hugo_0.111.3_Linux-64bit.tar.gz
 tar -xf hugo_0.111.3_Linux-64bit.tar.gz
 sudo mv ./hugo /usr/bin/
